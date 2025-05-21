cec69aa62ee369aa1624833da3ad5e0345931b74
Priyanshu Tiwari <priyanshutiwari878@yahoo.com>
2025-05-21 20:34:07 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,8 @@ import dotenv from "dotenv";
 dotenv.config({ path: ".env" });
 import messageRouter from "./routes/messages.routes";
 import aiChatRouter from "./routes/aichat.routes";
+import cronJobRouter from "./routes/cron.routes
+
 
 console.log(process.env.PORT);
 const app = express();
@@ -15,5 +17,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1/message", messageRouter);
 app.use("/api/v1/ai-chat", aiChatRouter);
+app.use("/api/v1/cron", cronJobRouter)
 
 export default app;
