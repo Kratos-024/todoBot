7ddbf9a9147042f704ac72b797c575d0f2d6b243
Priyanshu Tiwari <priyanshutiwari878@yahoo.com>
2025-05-21 20:40:22 +0530

    Create cronJob.controller.ts

---
+++ src/controllers/cronJob.controller.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express";
+import axios from "axios";
+import { asyncHandler } from "../utils/AsyncHandler";
+import { success } from "../utils/ApiResponse";
+import { badRequest } from "../utils/ApiError";
+
+export const sendPingRequest = asyncHandler(async (_req: Request, res: Response) => {
+  try {
+    
+    return res.status(200).send(success("Ping request successful", "response.data"));
+  } catch (error: any) {
+    return res.status(400).send(badRequest("Ping request failed", error.message));
+  }
+});
