7fe54a0aebde7190961c340ca4633cd7e1dc35e6
Priyanshu Tiwari <priyanshutiwari878@yahoo.com>
2025-05-21 20:32:27 +0530

    Update user.route.ts

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,7 +1,7 @@
-import { Router } from "express";
-import { stayUp } from "../controllers/cron.controller";
+//import { Router } from "express";
+//import { stayUp } from "../controllers/cron.controller";
 
-const cronJobRouter = Router();
+//const cronJobRouter = Router();
 
-cronJobRouter.route("/stayUptime").post(stayUp);
-export default cronJobRouter;
+//cronJobRouter.route("/////////////////////////tayUptime").post(stayUp);
+//export default cronJobRouter;
